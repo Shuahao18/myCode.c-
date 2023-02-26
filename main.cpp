ee5955bbf46ec0b18bceb4260bd440a9d02ff3a4
Shuahao18 <114126437+Shuahao18@users.noreply.github.com>
2023-02-26 17:14:36 +0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,14 +1,47 @@
 #include <iostream>
-#include <array>
+#include <windows.h>
 using namespace std;
 
 int main()
 {
-    int numberGrid[3][2] = {
-                            {1,2},
-                            {3,4},
-                            {5,6},
-                            };
-    cout << numberGrid[1][1] ;
+    int hour, minutes, seconds, a, err;
+    err=a=0;
+    while(err==0)
+    {
+        cout <<"Enter Hour: " <<endl;
+        cin >> hour;
+        cout <<"Enter Minutes: " <<endl;
+        cin >> minutes;
+        cout << "Enter a Seconds: " <<endl;
+        cin >> seconds;
+        if(hour<24 && minutes<60 && seconds<60)
+        err++;
+        else
+        system("cls");
+    }
+    while(a==0)
+    {
+        system ("cls");
+        cout <<hour<<":"<<minutes<<":"<<seconds<<endl;
+        Sleep(1000);
+        seconds++;
+        if(seconds>59);
+        {
+            seconds = 0;
+            minutes++;
+        }
+        if (minutes>59);
+        {
+            minutes = 0;
+            hour++;
+        }
+        if (hour>24);
+        {
+            hour = 0;
+        }
+
+
+    }
+
     return 0;
 }
